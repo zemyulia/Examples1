c3fd0d8cfe2234de85656da9d7247764466fa90f
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-24 16:16:00 +0300

    Add Example001 Hello Console project

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example001_HelloConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 Hello World!");
+        }
+    }
+}
