1ce2a8ed026f751875924328800fe33d8c6fa7c2
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-25 12:15:07 +0300

    Add Example 003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Example003_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; 
+            int b = 12; 
+            Console.WriteLine(a+b);
+        }
+    }
+}
