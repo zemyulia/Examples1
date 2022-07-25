3187e23104a38e8d0069a1936618f92b36b57f6d
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-25 13:45:05 +0300

    Add Example 003 Bases

---
+++ Example006_Base/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Example006_Base
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 6;
+            int d = 8;
+            int e = 4;
+
+            int max = a;
+            if (b > max) max = b;
+            if (c > max) max = c;
+            if (d > max) max = d;
+            if (e > max) max = e;
+
+            Console.Write("max = ");
+            Console.WriteLine(max);
+            
+                
+        
+        }
+    }
+    
+}
