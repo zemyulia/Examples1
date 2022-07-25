3fd1a99332f4c2427629d1ffcb567813d4a29a59
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-25 11:50:04 +0300

    Add Examlpe002 Hello User project

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example002_HelloUser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя:");
+            string username = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.Write(username);
+            Console.Write("!");
+            
+
+        }
+    }
+}
