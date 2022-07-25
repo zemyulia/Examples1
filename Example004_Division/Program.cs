5abcc21f8141e9d5987fd05a59bcf3b277cce971
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-25 12:25:05 +0300

    Add Example004 Division project

---
+++ Example004_Division/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Example004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = 17;
+            double numberB = 3;
+            Console.WriteLine(numberA/numberB);
+        }
+    }
+}
