f8c991ef72a68f4bc22fc2e4196619665a59ddc2
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-25 13:26:25 +0300

    Add If-Else construction

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Example005_IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите Ваше имя:");
+            string Username = Console.ReadLine();
+
+            if (Username.ToLower() == "маша")
+            {
+                Console.WriteLine("Ура, это же МАША!");
+            }
+
+            else
+            {
+                Console.Write("Привет,");
+                Console.WriteLine(Username);
+            }
+        }
+    }
+}
