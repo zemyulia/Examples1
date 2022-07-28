d0fa4551c6ca0c9ee0a5a54ebd1db0aaeb0f9f53
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 16:22:08 +0300

    Add Example 011 ArrayLibrary

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Example011_ArrayLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void FillArray(int[] collection)
+            {
+                int length = collection.Length;
+                int index = 0;
+
+                while(index<length)
+                {
+                    collection[index] = new Random().Next(1, 10);
+                    index++;
+                }
+            }
+            
+            void PrintArray(int[] col)
+            {
+                int count = col.Length;
+                int position = 0;
+
+                while(position<count)
+                {
+                    Console.WriteLine(col[position]);
+                    position++;
+                }
+            }
+
+            int IndexOf(int[] collection, int find)
+            {
+                int count = collection.Length;
+                int index = 0;
+                int position = 0;
+
+                while(index<count)
+                {
+                    if(collection[index] == find)
+                    {
+                        position = index;
+                        break;
+                    }
+                    index++;
+                }
+                return position;
+            }
+
+            
+            int[] array = new int[10];
+
+            FillArray(array);
+
+            array[4] = 4;
+            array[6] = 4;
+            PrintArray(array);
+
+            Console.WriteLine();
+            int Pos = IndexOf(array, 4);
+
+            Console.WriteLine(Pos);
+        }
+    }
+}
