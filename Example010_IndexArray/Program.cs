745ea4211834a28544dbcde9025b9409ae011d8e
Yuliia Melnik <yuliiamelnik@MacBook-Air.Dlink>
2022-07-28 15:48:49 +0300

    Add Example 010 finding the index.

---
+++ Example010_IndexArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Example010_IndexArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {10, 82, 19, 36, 4, 25, 64, 7, 82, 19};
+            int n = array.Length;
+            int find = 19; 
+
+            int index = 0;
+
+            while (index < n)
+            {
+                
+                if(array[index] == find)
+                {
+                    Console.WriteLine(index);
+                    break;
+                }
+
+            index++;
+            }
+        }
+    }
+}
